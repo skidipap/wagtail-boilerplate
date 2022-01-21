78b8f276a0e0d57a40c986e658055f4f29b220ed
skidipap <mad.skidipap@gmail.com>
2022-01-21 11:16:06 +0700

    :tada: Wagtail Boilerplate
    Minimal Wagtail Boilerplate

---
+++ web/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-pwe(l%p%7i4g(0wy11yze!w+!9!(21_c^)x!+ik1x4esaaf0#&'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
